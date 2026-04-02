e029752aff384b59d598b49fa0c1120dab8d8581
wbuziak <willbuziak@gmail.com>
2026-04-02 18:19:34 +0000

    shell script to prepare simulations

---
+++ resources/progs/src/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export CUR_DIR=$(pwd)
+export BMK=$1 # benchmark
+export SIZE=$2 # simulation size (simlarge, simsmall)
+export META_SIZE=$3 # metadata cache size
+export ARCH=$4 # architecture / name / mem-module we are testing (config file)
+
+# redirect the stats to a folder with the following naming convention
+RESULTS_DIR=$CUR_DIR/results/$BMK-$SIZE/$ARCH-$META_SIZE
+
+$CUR_DIR/build/X86/gem5.opt -d $RESULTS_DIR $CUR_DIR/configs/example/gem5_library/$ARCH --benchmark $BMK --size $SIZE --metadata_cache_size $META_SIZE --cache_mac
+
+# save the config file
+cp $CUR_DIR/configs/example/gem5_library/$ARCH $RESULTS_DIR/
