cab4b9ef48d42438d129a68b8983a45792e36a61
server6 <willbuziak@gmail.com>
2026-04-03 21:12:32 +0000

    config

--- resources/progs/src/run.sh
+++ resources/progs/src/run.sh
@@ -4,12 +4,12 @@ export CUR_DIR=$(pwd)
 export BMK=$1 # benchmark
 export SIZE=$2 # simulation size (simlarge, simsmall)
 export META_SIZE=$3 # metadata cache size
-export ARCH=$4 # architecture / name / mem-module we are testing (config file)
+export CONFIG=$4 # architecture / name / mem-module we are testing (config file)
 
 # redirect the stats to a folder with the following naming convention
-RESULTS_DIR=$CUR_DIR/results/$BMK-$SIZE/$ARCH-$META_SIZE
+RESULTS_DIR=$CUR_DIR/results/$BMK-$SIZE/$CONFIG-$META_SIZE
 
-$CUR_DIR/build/X86/gem5.opt -d $RESULTS_DIR $CUR_DIR/configs/example/gem5_library/$ARCH --benchmark $BMK --size $SIZE --metadata_cache_size $META_SIZE --cache_mac
+$CUR_DIR/build/X86/gem5.opt -d $RESULTS_DIR $CUR_DIR/configs/example/gem5_library/$CONFIG --benchmark $BMK --size $SIZE --metadata_cache_size $META_SIZE --cache_mac
 
 # save the config file
-cp $CUR_DIR/configs/example/gem5_library/$ARCH $RESULTS_DIR/
+cp $CUR_DIR/configs/example/gem5_library/$CONFIG $RESULTS_DIR/
